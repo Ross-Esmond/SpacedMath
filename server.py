5007a00af5bbf6a616e9073dd66fc380c0d478c2
Ross Esmond <r.esmond@protonmail.com>
2021-01-03 09:20:50 -0600

    feat: allowed cross-origin requests from all origins
    This is a temporary fix that I should change later. For security purposes, it should only allow requests from `SpacedMath.com`.

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from generate import problem 
 
 application = Flask(__name__)
-CORS(application, origins='http://spacedmath.com/')
+CORS(application)
 api = Api(application)
 
 class Problems(Resource):
