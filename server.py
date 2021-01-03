50828133097ec207f19fa7bd8e3f5968b17917fb
Ross Esmond <r.esmond@protonmail.com>
2021-01-03 08:58:38 -0600

    fix: allowed cross-origin request from spacedmath.com

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse, request
+from flask_cors import CORS
 from generate import problem 
 
 application = Flask(__name__)
+CORS(application, origins='http://spacedmath.com/')
 api = Api(application)
 
 class Problems(Resource):
