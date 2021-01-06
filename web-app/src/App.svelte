909396ae67e42e3cba203f1ec57d218264dc43fa
Ross Esmond <r.esmond@protonmail.com>
2021-01-06 11:10:45 -0600

    feat: added ability to generate new problems based on skills
    * Also added Skeleton css to better format the checkbox form and "generate" button.
    * Added code to make requests to localhost when running on localhost to improve debugging.

--- web-app/src/App.svelte
+++ web-app/src/App.svelte
@@ -1,27 +1,25 @@
 <script>
-	import { afterUpdate } from 'svelte';
+	let host = 'http://pure-springs-76606.herokuapp.com'
+  if (location.hostname === "localhost") {
+		host = 'http://127.0.0.1:8000' 
+	}
+
+	import { beforeUpdate, afterUpdate } from 'svelte';
 
 	let options = []
 	let problem = ''
 
-	fetch('http://pure-springs-76606.herokuapp.com/options')
+	fetch(`${host}/options`)
 	.then(response => response.json())
 	.then(json => {
-		options = json.options
+		options = json.options.map(name => ({ name, value: false }))
 	})
 
-	fetch('http://pure-springs-76606.herokuapp.com/', {
-		method: 'POST',
-		body: JSON.stringify({
-			required: ['mult', 'div'],
-			allowed: { power: true, addition: true }
-		}),
-		headers: {
-			'content-type': 'application/json'
+	beforeUpdate(() => {
+		let span = document.querySelector('.math-problem')
+		if (span != null) {
+			span.innerHTML = problem
 		}
-	}).then(response => response.json())
-	.then(json => {
-		problem = `\\(${json.problem}\\)`
 	})
 
 	afterUpdate(() => {
@@ -29,15 +27,33 @@
 			MathJax.typeset()
 		}
 	})
+
+	function generate() {
+		fetch(host, {
+			method: 'POST',
+			body: JSON.stringify({
+				required: options.filter(op => op.value).map(op => op.name),
+				allowed: { power: true, addition: true }
+			}),
+			headers: {
+				'content-type': 'application/json'
+			}
+		}).then(response => response.json())
+			.then(json => {
+				problem = `\\(${json.problem}\\)`
+			})
+	}
 </script>
 
 <div class="contents">
   <div class="options">
 		{#each options as option}
-		<div>
-			<input id="option-{option}" type="checkbox" name="option-{option}" /> <label for="option-{option}">{option}</label>
-		</div>
+		<label>
+			<input id="option-{option.name}" type="checkbox" bind:checked={option.value} /> <span class="label-body">{option.name}</span>
+		</label>
 		{/each}
+		<button on:click={generate}>generate</button>
 	</div>
   <span class="math-problem">{problem}</span>
 </div>
+
