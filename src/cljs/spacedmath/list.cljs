5ae630e2203b927efa7ae04ff7819e9c029e95f8
Ross Esmond <ross.alexander.esmond@gmail.com>
2021-06-19 09:42:24 -0500

    chore: added two more, relatively standard, polynomial differenetiation problems

--- src/cljs/spacedmath/list.cljs
+++ src/cljs/spacedmath/list.cljs
@@ -6,4 +6,6 @@
     [:exp 5]
     [:add [:mult 5.2 :x] 2.3]
     [:add [:mult [:div 7 4] [:power :x 2]] [:mult -3 :x] 12]
-    [:add [:add [:mult 2 [:power :t 3]] [:mult -3 [:power :t 2]] [:mult -4 :t]]]])
+    [:add [:add [:mult 2 [:power :t 3]] [:mult -3 [:power :t 2]] [:mult -4 :t]]]
+    [:add [:add [:mult 1.4 [:power :t 5]] [:mult -2.5 [:power :t 2]] 6.7]]
+    [:add [:add [:power :x 2] [:mult -2 [:power :x 3]]]]])
