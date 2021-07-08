2054ca2639d5d284550817ff2a055a32d5aaf0bb
Ross Esmond <ross.alexander.esmond@gmail.com>
2021-07-08 18:30:58 -0500

    chore: print "listening..." line on go server startup

--- src/go/main.go
+++ src/go/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
+	"fmt"
 )
 
 func main() {
 	http.Handle("/js/", http.FileServer(http.Dir("resources/public/")))
 	http.Handle("/", http.FileServer(http.Dir("static/")))
 
+	fmt.Println("listening on localhost:3000")
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
